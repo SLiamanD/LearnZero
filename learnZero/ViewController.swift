2ee46bd241ef152b5c500854481bef56551010ba
sliamand <sliamand@mail.ru>
2022-07-19 20:43:37 +0300

    third lesson repeat teacher DESING
    repeat desing third lesson on THE SWIFT DEVELOPERS on youtube

--- learnZero/ViewController.swift
+++ learnZero/ViewController.swift
@@ -8,38 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var avatarOutlet: UIImageView!
-    
-    
-    
+
+    @IBOutlet weak var hiLabel: UILabel!
+    @IBOutlet weak var tapOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("this is a new feature")
-        // )))
-        // add a comment
-        // add a new comment
-        // and new
-        // and new and new 
-        
-        avatarOutlet.layer.cornerRadius = avatarOutlet.frame.size.width / 7
-        avatarOutlet.clipsToBounds = true
-        avatarOutlet.layer.borderWidth = 2.0
-        avatarOutlet.layer.borderColor = UIColor.lightGray.cgColor
-
+        tapOutlet.backgroundColor = .green
+        tapOutlet.frame.size.width = 50
+        tapOutlet.frame.size.height = 50
+//        tapOutlet.layer.cornerRadius = 16
+//        tapOutlet.layer.masksToBounds = false
+        tapOutlet.layer.cornerRadius = tapOutlet.frame.size.height / 3
     }
     override func viewWillAppear(_ animated: Bool) {
-//        title = textFromVC
-
-        
-        
-        // Navigation Bar:
-//        navigationController?.navigationBar.barTintColor = UIColor.gray
-
-        // back button chose color
-        
-        
-        self.navigationController?.navigationBar.tintColor = UIColor.black
-
     }
-
+    @IBAction func tapAction(_ sender: UIButton) {
+    }
 }
