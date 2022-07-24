c022266a66cd58ba9aab4f559a3992db42cf1939
SLiamanD <get>
2022-07-24 18:40:12 +0300

    clear project to new home work

--- LearnZero/Controlllers/ViewController.swift
+++ LearnZero/Controlllers/ViewController.swift
@@ -9,47 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var hiLabel: UILabel!
-    @IBOutlet weak var startGameOutlet: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        hiLabel.isHidden = true
-        startGameOutlet.isHidden = true
-        startGameOutlet.setTitle("продолжить", for: .normal)
-        startGameOutlet.frame.size.width = 280
-        startGameOutlet.frame.size.height = 75
-        startGameOutlet.layer.cornerRadius = 12
-        startGameOutlet.layer.masksToBounds = true
-        startGameOutlet.backgroundColor = .cyan
-        startGameOutlet.center = view.center
+
     }
-    override func viewWillAppear(_ animated: Bool) {
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        //Variable to store alertTextField
-        var firstTextField = UITextField()
-        var secondTextField = UITextField()
-        let alert = UIAlertController(title: "Добро пожаловать!", message: "давайте знакомиться", preferredStyle: .alert)
-        alert.addTextField { alertTextField in
-            alertTextField.placeholder = "введите пожалуйста ваше имя"
-            
-            //Copy alertTextField in local variable to use in current block of code
-            firstTextField = alertTextField
-        }
-        
-        var tappedOnTextFieldAction = false
-        let action = UIAlertAction(title: "познакомиться", style: .default) { action in
-            tappedOnTextFieldAction = true
-            self.hiLabel.text! = "Привет " + firstTextField.text! + "!"
-            if tappedOnTextFieldAction == true {
-                self.hiLabel.isHidden = false
-                self.startGameOutlet.isHidden = false
-            }
-        }
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
-    }
+
     
 }
 
