de4f92e50189c0ecba3117dec264560ac1d9a2f0
sliamand <sliamand@mail.ru>
2022-07-27 15:36:28 +0300

    TSDUIKit L24 repeat i made a red square and set the constraints with code by func NSLayoutConstraint

--- LearnZero/controllers/ViewController.swift
+++ LearnZero/controllers/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     var view1: UIView =  {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         view.backgroundColor = UIColor.red
         return view
         
@@ -22,6 +22,50 @@ class ViewController: UIViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(view1)
+        createView1Constant()
     }
+    
+    func createView1Constant() {
+        NSLayoutConstraint(item: view1,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .trailingMargin,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .trailingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .leadingMargin,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .topMargin,
+                           multiplier: 1,
+                           constant: 80).isActive = true
+        NSLayoutConstraint(item: view1,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+    }
+    
+    
+    
 
 }
