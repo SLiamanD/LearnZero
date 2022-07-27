c5816cba8fe378c8df70a178cfaaf15a43231771
SLiamanD <sliamand@mail.ru>
2022-07-27 11:50:36 +0300

    LearnZero with all working copy

---
+++ LearnZero/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  LearnZero
+//
+//  Created by sliamand on 27.07.2022.
+//
+
+import UIKit
+ 
+class ViewController: UIViewController {
+ 
+//    var view1: UIView {
+//        let view = UIView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
+//        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+//        view.backgroundColor = UIColor.red
+//        return view
+//    }
+ 
+ 
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+ 
+ 
+
+}
---
+++ copy/LearnZero UIKit original copy zero status /LearnZero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnZero
+//
+//  Created by sliamand on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
