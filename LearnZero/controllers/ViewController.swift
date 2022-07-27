776f191b3671365c55502fe3028b7084bdeea451
sliamand <sliamand@mail.ru>
2022-07-27 17:44:08 +0300

    TSDUIKit L24 repeat - apple VFL constraints added

--- LearnZero/controllers/ViewController.swift
+++ LearnZero/controllers/ViewController.swift
@@ -9,18 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var view1: UIView =  {
+    var viewRed: UIView =  {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         view.backgroundColor = UIColor.red
         return view
     }()
     
-    var view2: UIView =  {
+    var viewBlue: UIView =  {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         view.backgroundColor = UIColor.red
         return view
     }()
@@ -29,71 +27,23 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(view1)
-        view.addSubview(view2)
-        createView1Constant()
-        createView2Constant()
-    }
-    
-    func createView1Constant() {
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerY,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerY,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 200).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 200).isActive = true
-    }
-    
-    func createView2Constant() {
-        NSLayoutConstraint(item: view2,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view2,
-                           attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .top,
-                           multiplier: 1,
-                           constant: -8).isActive = true
-        NSLayoutConstraint(item: view2,
-                           attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-        NSLayoutConstraint(item: view2,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 100).isActive = true
+        view.addSubview(viewRed)
+        view.addSubview(viewBlue)
+        
+        let viewVFL = ["viewRed":viewRed,"viewBlue":viewBlue]
+        let metrics = ["height":100, "width": view.bounds.size.width / 3, "top": view.bounds.size.height / 2 ]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewRed(height)]|",
+                                                          options: [],
+                                                          metrics: metrics,
+                                                          views: viewVFL))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewBlue(height)]|",
+                                                          options: [],
+                                                          metrics: metrics,
+                                                          views: viewVFL))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[viewRed(width)]-(50)-[viewBlue(width)]-|",
+                                                          options: [],
+                                                          metrics: metrics,
+                                                          views: viewVFL))
     }
 }
