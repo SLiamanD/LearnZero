845be27b717e71d2c29c04f60d08ba380002e66a
sliamand <sliamand@mail.ru>
2022-07-27 14:57:40 +0300

    edit display name of applecation

--- LearnZero/controllers/ViewController.swift
+++ LearnZero/controllers/ViewController.swift
@@ -9,22 +9,19 @@ import UIKit
  
 class ViewController: UIViewController {
  
-//    var view1: UIView {
-//        let view = UIView()
-//        view.translatesAutoresizingMaskIntoConstraints = false
-//        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-//        view.backgroundColor = UIColor.red
-//        return view
-//    }
+    var view1: UIView =  {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        view.backgroundColor = UIColor.red
+        return view
+        
+    }()
  
  
  
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
     }
- 
- 
 
 }
