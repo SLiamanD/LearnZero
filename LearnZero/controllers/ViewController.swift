87e733b515187e3e149fcd9d740eb8c443da27c7
sliamand <sliamand@mail.ru>
2022-07-27 17:06:51 +0300

    TSDUIKit L24 repeat -  add constrains by code to attach second red square to first red square

--- LearnZero/controllers/ViewController.swift
+++ LearnZero/controllers/ViewController.swift
@@ -6,24 +6,33 @@
 //
 
 import UIKit
- 
+
 class ViewController: UIViewController {
- 
+    
     var view1: UIView =  {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         view.backgroundColor = UIColor.red
         return view
-        
     }()
- 
- 
- 
+    
+    var view2: UIView =  {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
+        view.backgroundColor = UIColor.red
+        return view
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(view1)
+        view.addSubview(view2)
         createView1Constant()
+        createView2Constant()
     }
     
     func createView1Constant() {
@@ -55,17 +64,37 @@ class ViewController: UIViewController {
                            attribute: .notAnAttribute,
                            multiplier: 1,
                            constant: 200).isActive = true
-//        NSLayoutConstraint(item: view1,
-//                           attribute: .height,
-//                           relatedBy: .equal,
-//                           toItem: view,
-//                           attribute: .width,
-//                           multiplier: 1,
-//                           constant: 0).isActive = true
-        
     }
     
-    
-    
-
+    func createView2Constant() {
+        NSLayoutConstraint(item: view2,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: view1,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        NSLayoutConstraint(item: view2,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: view1,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: -8).isActive = true
+        NSLayoutConstraint(item: view2,
+                           attribute: .width,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .notAnAttribute,
+                           multiplier: 1,
+                           constant: 100).isActive = true
+        NSLayoutConstraint(item: view2,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: nil,
+                           attribute: .notAnAttribute,
+                           multiplier: 1,
+                           constant: 100).isActive = true
+        
+    }
 }
