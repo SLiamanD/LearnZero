e0f3ce587081683e51eae19198136113b4cf7fb0
sliamand <sliamand@mail.ru>
2022-09-03 20:34:32 +0300

    TFS fix bug with pickerView

--- LearnZero/controllers/ViewController.swift
+++ LearnZero/controllers/ViewController.swift
@@ -12,17 +12,17 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
+    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        self.companies.keys.count
+        return self.companies.keys.count
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+      }
     
     //MARK: UIPickerViewDelegate
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        self.activityIndicator.startAnimating()
-        let selectedSymbol = Array(self.companies.values)[row]
-        self.requestQuote(for: selectedSymbol)
-        displayStockInfo(companyName: selectedSymbol)
-        
         
         return Array(self.companies.keys)[row]
     }
@@ -35,70 +35,15 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.companyNameLabel.text = "Tinkoff"
+        let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
+        let selectedSymbol = Array(self.companies.values)[selectedRow]
+        displayStockInfo(companyName: selectedSymbol)
         self.companyPickerView.dataSource = self
         self.companyPickerView.delegate = self
         self.activityIndicator.hidesWhenStopped = true
         activityIndicator.isHidden = false
         self.activityIndicator.startAnimating()
-        self.requestQuote(for: "APPL")
-        self.requestQuoteUpdate()
     }
-    
-    //MARK: pravate methods
-    
-    
-    private func requestQuote(for symbol: String) {
-        let url = URL(string: "https://cloud.iexapis.com/stable/stock/\(symbol)/quote?token=pk_aebcb09cb11e4309ad7cb286f55f57ac")!
-
-        let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                error == nil,
-                (response as? HTTPURLResponse)?.statusCode == 200,
-                let data = data
-            else {
-            print("! Network error")
-            return
-        }
-            self.parceQuote(data:data)
-            
-            // так можно распечатать весь json целиком - print(String(data:data, encoding: .utf8))
-            
-        }
-        
-        dataTask.resume()
-    
-    }
-    
-    
-    
-    private func parceQuote(data:Data) {
-        do {
-            let jsonObject = try JSONSerialization.jsonObject(with: data)
-            guard
-                let json = jsonObject as? [String:Any],
-                let companyName = json["companyName"] as? String
-
-
-            else {
-                print (" ! invalid JSON format")
-                return
-            }
-            print( "Company name is '\(companyName)' ")
-        } catch {
-            print ("! Json parsing error:" + error.localizedDescription)
-        }
-        
-    }
-    
-    private func requestQuoteUpdate() {
-        self.activityIndicator.startAnimating()
-        
-        let selectedRow = self.companyPickerView.selectedRow(inComponent: 0)
-        let selectedSymbol = Array(self.companies.values)[selectedRow]
-        self.requestQuote(for: selectedSymbol)
-    }
-    
 
     private func displayStockInfo(companyName: String) {
         self.activityIndicator.stopAnimating()
