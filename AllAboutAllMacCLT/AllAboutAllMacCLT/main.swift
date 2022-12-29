deb531a3bdcfde32110422d280ed7105aad1147c
SLiamanD <get>
2022-12-29 21:03:41 +0300

    algo

--- AllAboutAllMacCLT/AllAboutAllMacCLT/main.swift
+++ AllAboutAllMacCLT/AllAboutAllMacCLT/main.swift
@@ -5,7 +5,30 @@
 //  Created by Алексей Грачев on 24.12.2022.
 //
 
+// https://www.youtube.com/watch?v=9Gg0ikZka_w
+
+// https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/
+
+
+
 import Foundation
 
-print("Hello, World!")
+
+class Solution {
+    func maximumBags(_ capacities: [Int], _ rocks: [Int], _ additionalRocks: Int) -> Int {
+        var fullBagsCount = 0
+        var additionalRocks = additionalRocks
+        var reminders: [Int] = []
+        
+        
+        
+        return 0
+        print(0)
+        
+        
+    }
+}
+
+
+
 
